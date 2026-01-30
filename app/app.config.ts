4b503cb0a254536f4bb1346ed1ed34336ff264d9
kwaliyo <kwaliyo@gmail.com>
2026-01-30 12:35:30 +0100

    first commit

---
+++ app/app.config.ts
@@ -0,0 +1,76 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'green',
+      neutral: 'slate'
+    },
+    footer: {
+      slots: {
+        root: 'border-t border-default',
+        left: 'text-sm text-muted'
+      }
+    }
+  },
+  seo: {
+    siteName: 'Nuxt Docs Template'
+  },
+  header: {
+    title: '',
+    to: '/',
+    logo: {
+      alt: '',
+      light: '',
+      dark: ''
+    },
+    search: true,
+    colorMode: true,
+    links: [{
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/nuxt-ui-templates/docs',
+      'target': '_blank',
+      'aria-label': 'GitHub'
+    }]
+  },
+  footer: {
+    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    colorMode: false,
+    links: [{
+      'icon': 'i-simple-icons-discord',
+      'to': 'https://go.nuxt.com/discord',
+      'target': '_blank',
+      'aria-label': 'Nuxt on Discord'
+    }, {
+      'icon': 'i-simple-icons-x',
+      'to': 'https://go.nuxt.com/x',
+      'target': '_blank',
+      'aria-label': 'Nuxt on X'
+    }, {
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/nuxt/ui',
+      'target': '_blank',
+      'aria-label': 'Nuxt UI on GitHub'
+    }]
+  },
+  toc: {
+    // Title of the main table of contents
+    title: 'Table of Contents',
+    // Customize links
+    bottom: {
+      // Title of the bottom table of contents
+      title: 'Community',
+      // URL of your repository content folder
+      edit: 'https://github.com/nuxt-ui-pro/docs/edit/main/content',
+      links: [{
+        icon: 'i-lucide-star',
+        label: 'Star on GitHub',
+        to: 'https://github.com/nuxt/ui',
+        target: '_blank'
+      }, {
+        icon: 'i-lucide-book-open',
+        label: 'Nuxt UI docs',
+        to: 'https://ui.nuxt.com/getting-started/installation',
+        target: '_blank'
+      }]
+    }
+  }
+})
